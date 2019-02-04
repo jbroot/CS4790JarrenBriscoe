00e85cb6b3df9c73ce591a7806f08d87999753bc
Jarren Briscoe <jarren.briscoe@gmail.com>
2019-02-04 05:45:10 -0700

    granite

---
+++ GraniteWarehouse/GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GraniteWarehouse.Models;
+
+namespace GraniteWarehouse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductTypes> ProductTypes { get; set; }  
+    }
+}
