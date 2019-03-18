c001d599ebb1406fc247c234c8b8ce041041f4b4
Kylie Powers <kyliepowers1@mail.weber.edu>
2019-03-18 00:21:21 -0600

    Database Configuration

---
+++ EastAdvising/Models/College.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EastAdvising.Models
+{
+    public class College
+    {
+        public int CollegeId { get; set; }
+        public string CollegeName { get; set; }
+
+        //Navigation Properties
+        public virtual ICollection<Department> Departments { get; set; }
+    }
+}
